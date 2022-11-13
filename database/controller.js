8402da0e46365ac99be10a3663e2d9aad638b55e
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-11-13 23:12:38 +0600

    formData created successfull

--- database/controller.js
+++ database/controller.js
@@ -1,5 +1,8 @@
+/*Controller  */
+
 import Users from "../model/user";
 
+/* GET Request--> http://localhost:3000/api/users */
 export async function getUsers(req, res) {
   try {
     const users = await Users.find({});
@@ -7,5 +10,21 @@ export async function getUsers(req, res) {
     res.status(200).json(users);
   } catch (error) {
     res.status(404).json({ error: "Error while fetching data" });
+  }
+}
+
+/* POST Request--> http://localhost:3000/api/users */
+
+export async function createUser(req, res) {
+  try {
+    const formData = req.body;
+    if (!formData) {
+      return res.status(404).json({ error: "Form data not provided..!!!" });
+    }
+    Users.create(formData, (err, data) => {
+      return res.status(201).json(data);
+    });
+  } catch (error) {
+    return res.status(404).json({ error });
   }
 }
